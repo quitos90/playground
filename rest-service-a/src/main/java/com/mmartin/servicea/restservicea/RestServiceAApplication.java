5a83e84041e8566257982c9196c57e88e6c059bb
Marcos <martinalvarez.marcos@gmail.com>
2017-09-24 13:23:45 +0200

    Borrar

--- rest-service-a/src/main/java/com/mmartin/servicea/restservicea/RestServiceAApplication.java
+++ rest-service-a/src/main/java/com/mmartin/servicea/restservicea/RestServiceAApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceAApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hola");
 		SpringApplication.run(RestServiceAApplication.class, args);
 	}
 }
